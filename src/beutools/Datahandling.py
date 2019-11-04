19b0d3cf457be48975ee41b9e136be040c87e5bb
Benno Lorenz <bennolor@gmail.com>
2019-11-04 03:05:56 +0100

    Minor changes to Data tools

--- src/beutools/Datahandling.py
+++ src/beutools/Datahandling.py
@@ -1,9 +1,21 @@
+import os
+
+
+#FIXME
 def createPath(relPath: str) -> str:
     scriptDir = os.path.dirname(__file__)
     return os.path.join(scriptDir, relPath)
 
 
 def createSavePath(fileName: str) -> str:
+    return createPath('../res/out/' + fileName)
+
+
+def createSavePath(fileName: str) -> str:
+    return createPath('../res/out/' + fileName)
+
+
+def createGUIPath(fileName: str) -> str:
     return createPath('../res/out/' + fileName)
 
 
